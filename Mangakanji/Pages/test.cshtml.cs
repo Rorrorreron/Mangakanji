77f163c79a0057166fe25c942a886f909934a74e
Rorrorreron <rodrigo.castillo1816@gmail.com>
2022-09-01 23:44:40 -0400

    tercer commit

--- Mangakanji/Pages/test.cshtml.cs
+++ Mangakanji/Pages/test.cshtml.cs
@@ -1,3 +1,4 @@
+using Mangakanji.NEGOCIO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace Mangakanji.Pages
 {
     public class testModel : PageModel
     {
+        private readonly ICalculo _calculo;
+
+        public testModel(ICalculo calculo)
+        {
+            _calculo = calculo;
+        }
+
+        [BindProperty]
+        public int Numero1 { get; set; }
+        [BindProperty]
+        public int Numero2 { get; set; }
         public void OnGet()
         {
+            //this.Numero1 = 123;
+            //this.Numero2 = 321;
+        }
+        [BindProperty]
+        public int? Resultado { get; set; }
+        public void OnPost()
+        {
+            
+            var resultado = _calculo.Operacion(this.Numero1, this.Numero2);
+            
         }
     }
 }
